99a73f86f854eb2f588fa9ba6f4f5a3a1ea4de26
bvernon <ibspoof@gmail.com>
2013-05-28 15:31:57 -0700

    Added new features:
    * Support for setting iScroll specific options via App Controller
    * Support for iScroll 'refresh' and other functions via App Controller
    * iScroll Options can be set via App Controller

--- src/ng-iscroll.js
+++ src/ng-iscroll.js
@@ -5,16 +5,23 @@ angular.module('ng-iscroll', []).directive('ngIscroll', function ()
         restrict: 'A',
         link: function (scope, element, attr)
         {
-
             // default timeout
             var ngiScroll_timeout = 5;
 
+
             // default options
             var ngiScroll_opts = {
-                nap: true,
+                snap: true,
                 momentum: true,
                 hScrollbar: false
             };
+
+            // scroll key /id
+            var scroll_key = attr.ngIscroll;
+
+            if (scroll_key === '') {
+                scroll_key = attr.id;
+            }
 
             // if ng-iscroll-form='true' then the additional settings will be supported
             if (attr.ngIscrollForm !== undefined && attr.ngIscrollForm == 'true') {
@@ -29,15 +36,26 @@ angular.module('ng-iscroll', []).directive('ngIscroll', function ()
                 };
             }
 
+            if (scope.$parent.myScrollOptions) {
+                for (var i in scope.$parent.myScrollOptions) {
+                    if (i === scroll_key) {
+                        for (var k in scope.$parent.myScrollOptions[i]) {
+                            ngiScroll_opts[k] = scope.$parent.myScrollOptions[i][k];
+                        }
+                    } else {
+                        ngiScroll_opts[i] = scope.$root.myScrollOptions[i];
+                    }
+                }
+            }
+
             // iScroll initialize function
             function setScroll()
             {
-                myScroll = new iScroll(element[0], ngiScroll_opts);
-            }
+                if (scope.$parent.myScroll === undefined) {
+                    scope.$parent.myScroll = [];
+                }
 
-            // support for old method of setting timeout
-            if (attr.ngIscroll !== "") {
-                ngiScroll_timeout = attr.ngIscroll;
+                scope.$parent.myScroll[scroll_key] = (new iScroll(element[0], ngiScroll_opts));
             }
 
             // new specific setting for setting timeout using: ng-iscroll-timeout='{val}'
@@ -46,7 +64,7 @@ angular.module('ng-iscroll', []).directive('ngIscroll', function ()
             }
 
             // watch for 'ng-iscroll' directive in html code
-            scope.$watch(attr.ngIscroll, function (value)
+            scope.$watch(attr.ngIscroll, function ()
             {
                 setTimeout(setScroll, ngiScroll_timeout);
             });
